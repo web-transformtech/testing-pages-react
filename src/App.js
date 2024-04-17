1eade4f863440bdda31ed9745074586943e82f3d
balamuruganUI <balamurugan.a@rvsgroup.com>
2024-04-17 17:07:26 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes, BrowserRouter, HashRouter } from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/testing-pages-react">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          Hello
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
