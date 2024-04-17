2edf6a341f7743dfb3131057444dabbd1049c8b2
balamuruganUI <balamurugan.a@rvsgroup.com>
2024-04-17 16:55:49 +0530

    changess

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './Contact';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/testing-pages-react">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
